0da5ec2da35d002239b6fcf2cca657eaeed13bd6
Andrew Nguonly <andrewnguonly@gmail.com>
2023-11-19 10:59:13 -0800

    Reformat code.

--- src/components/ChatBar.tsx
+++ src/components/ChatBar.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { Box, IconButton, LinearProgress, TextField } from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";import "./ChatBar.css";
+import "./ChatBar.css";
 
 
 const ChatBar: React.FC = () => {
@@ -17,7 +17,6 @@ const ChatBar: React.FC = () => {
   const handleSendButtonClick = async () => {
     setLoading(true);
     setSubmitDisabled(true);
-
     chrome.runtime.sendMessage({ prompt: prompt });
   };
 
@@ -27,23 +26,10 @@ const ChatBar: React.FC = () => {
       setSubmitDisabled(false);
       setCompletion(msg.answer);
     }
-    
   });
 
   useEffect(() => {
-    
     chrome.runtime.onMessage.addListener(handleBackgroundMessage);
-
-    // fetch page contents
-    // chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
-    //   if (tabs[0].id) {
-    //     var port = chrome.tabs.connect(tabs[0].id, {name: "channelName"});
-    //     port.postMessage({});
-    //     port.onMessage.addListener((msg) => {
-    //       chrome.runtime.sendMessage({ context: msg.contents });
-    //     });
-    //   }
-    // });
   });
 
   return (
@@ -60,7 +46,7 @@ const ChatBar: React.FC = () => {
           disabled={submitDisabled}
           onClick={handleSendButtonClick}
         >
-          <img alt="" src="../assets/wand_32.png"/>
+          <img alt="" src="../assets/wand_32.png" />
         </IconButton>
       </Box>
       <Box className="chat-box">
@@ -71,7 +57,7 @@ const ChatBar: React.FC = () => {
           value={completion}
         />
       </Box>
-      {loading && <LinearProgress/>}
+      {loading && <LinearProgress />}
     </Box>
   );
 }
--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,41 +1,27 @@
-import {ChatRestModule} from "@mlc-ai/web-llm";
-import { Buffer } from 'buffer';
+import { ChatRestModule } from "@mlc-ai/web-llm";
 
-// TODO: Surface this as an option to the user 
-const useWebGPU = false;
 
-var cm: ChatRestModule;
-if (!useWebGPU) {
-    cm = new ChatRestModule();
-}
+const cm = new ChatRestModule();
+var context = "";
 
 // Set reponse callback for chat module
-const generateProgressCallback = (_step: number, message: string) => {
-    // send the answer back to the content script
-    console.log(`generating progres... ${_step}`);
-    console.log(`messsage: ${message}`);
-    chrome.runtime.sendMessage({ answer: message });
+const generateProgressCallback = (step: number, message: string) => {
+  // send the answer back to the content script
+  console.log(`messsage (${step}): ${message}`);
+  chrome.runtime.sendMessage({ answer: message });
 };
 
-var context = "";
 chrome.runtime.onMessage.addListener(async function (request) {
-    // check if the request contains a message that the user sent a new message
-    if (request.prompt) {
-        var inp = request.prompt;
-        if (context.length > 0) {
-            inp = "Use only the following context when answering the question at the end. Don't use any other knowledge.\n"+ context + "\n\nQuestion: " + request.input + "\n\nHelpful Answer: ";
-        }
-        console.log("Input:", inp);
-        await cm.generate(inp, generateProgressCallback)
-          .then((response) => {
-            console.log(`got response: ${response}`);
-          })
-          .catch((error) => {
-            console.log(`error: ${error}`);
-          });
+  if (request.prompt) {
+    var prompt = request.prompt;
+    if (context.length > 0) {
+      prompt = "Use only the following context when answering the question at the end. Don't use any other knowledge.\n" + context + "\n\nQuestion: " + request.prompt + "\n\nHelpful Answer: ";
     }
-    if (request.context) {
-        context = request.context;
-        console.log("Got context:", context);
-    }
+    console.log(`Prompt: ${prompt}`);
+    await cm.generate(prompt, generateProgressCallback);
+  }
+  if (request.context) {
+    context = request.context;
+    console.log(`Received context: ${context}`);
+  }
 });
